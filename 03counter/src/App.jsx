721fdb9cb1f7b758a6a8355a9cbd8d3cf8828ac3
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-13 04:39:13 +0000

    change2

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+//hook in react
 function App() {
 
   let [counter, setCounter] = useState(5)

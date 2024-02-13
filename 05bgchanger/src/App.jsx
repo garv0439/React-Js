2f00dd32fb1289d2af3bce9fc80a46681291911a
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-13 09:00:34 +0000

    appchange

--- 05bgchanger/src/App.jsx
+++ 05bgchanger/src/App.jsx
@@ -11,21 +11,61 @@ function App() {
       >
         <div className="fixed flex flex-wrap justify-center bottom-48 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-large"
             style={{backgroundColor: "red"}}
             >red</button>
-             <button className="outline-none px-4 py-1 rounded-full text-white shadow-large"
-            style={{backgroundColor: "red"}}
-            >red</button>
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-large"
-            style={{backgroundColor: "red"}}
-            >red</button>
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-large"
-            style={{backgroundColor: "red"}}
-            >red</button>
-            <button className="outline-none px-4 py-1 rounded-full text-white shadow-large"
-            style={{backgroundColor: "red"}}
-            >red</button>
+             <button
+             onClick={() => setColor("green")}
+             className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            style={{backgroundColor: "green"}}
+            >green</button>
+            <button 
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            style={{backgroundColor: "blue"}}
+            >blue</button>
+            <button 
+            onClick={() => setColor("violet")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            style={{backgroundColor: "violet"}}
+            >violet</button>
+            <button 
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-black  shadow-large"
+            style={{backgroundColor: "yellow"}}
+            >yellow</button>
+             <button 
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            style={{backgroundColor: "black"}}
+            >black</button>
+             <button 
+            onClick={() => setColor("grey")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-large"
+            style={{backgroundColor: "grey"}}
+            >grey</button>
+            <button 
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-large"
+            style={{backgroundColor: "olive"}}
+            >olive</button>
+            <button 
+            onClick={() => setColor("lavender")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-large"
+            style={{backgroundColor: "lavender"}}
+            >lavender</button>
+            <button 
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-large"
+            style={{backgroundColor: "pink"}}
+            >pink</button>
+            <button 
+            onClick={() => setColor("white")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-large"
+            style={{backgroundColor: "white"}}
+            >white</button>
           </div>
           </div>
       </div>

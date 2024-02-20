249669e779d311dea93084985e26dc21151c6d50
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-20 11:06:52 +0000

    commit4

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,39 @@
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice";
+import { Footer } from './components';
+import {Outlet} from 'react-router-dom'
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-       <h1>hi</h1>
-    </>
-  )
+  const [Loading, setLoading] =useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }
+  ,[])
+
+  return !Loading ?(
+    <div className='min-h-screen flex flex-wrap vcontent-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+       TODO:   <Outlrt />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App

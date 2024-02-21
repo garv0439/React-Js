5148a94ba1d08fcdbfff765a54b4f067fc9f04bd
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-21 05:35:40 +0000

    commit7

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -3,7 +3,7 @@ import {useDispatch} from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
 import { login, logout } from "./store/authSlice";
-import { Footer } from './components';
+import { Footer, Header } from './components'
 import {Outlet} from 'react-router-dom'
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-       TODO:   <Outlrt />
+       TODO:   <Outlet />
         </main>
         <Footer />
       </div>

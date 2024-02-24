bc6b1ddbd830a9e43c241688da20999ce709eb3f
Garv Kadia <145740289+garv0439@users.noreply.github.com>
2024-02-24 09:27:20 +0530

    Update App.jsx

--- 12MEGABLOG/src/App.jsx
+++ 12MEGABLOG/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-        TODO:  <Outlet />
+             TODO:   <Outlrt />
         </main>
         <Footer />
       </div>
